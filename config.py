8c6733ee4aa46c1c77ada5126528b8cace0c4b0a
amarSeb <amarsebti04@gmail.com>
2025-08-09 20:43:35 +0100

    Create config.py
    # config.py
    
    # توكن البوت الخاص بك
    BOT_TOKEN = "8218899898:AAEXxoTyv3tG6aiVz5c5rEX2wxyvFwttN9w"
    
    # آي دي المدير
    ADMIN_ID = 5130562279
    
    # رابط الدعوة للقناة الخاصة
    CHANNEL_INVITE_LINK = "https://t.me/+jWHdmsigzz5hZDRk"
    
    # بيانات الدفع
    PAYPAL_EMAIL = "mimoucanadien01@gmail.com"
    BARIDIMOB_NUMBER = "00799999001806537421"
    USDT_ADDRESS_1 = "0x1aaa53596e4a3fc411bdf5c8c9c315b2253bcbda"
    USDT_ADDRESS_2 = "0x4ec85ef2dca621ed2cdd0dbed7a9ee2d5d688f20"
    
    # الأسعار
    SUBSCRIPTION_PRICE_USD = "200$"
    SUBSCRIPTION_PRICE_DZD = "48000 Da"

---
+++ config.py
@@ -0,0 +1,20 @@
+# config.py
+
+# توكن البوت الخاص بك
+BOT_TOKEN = "8218899898:AAEXxoTyv3tG6aiVz5c5rEX2wxyvFwttN9w"
+
+# آي دي المدير
+ADMIN_ID = 5130562279
+
+# رابط الدعوة للقناة الخاصة
+CHANNEL_INVITE_LINK = "https://t.me/+jWHdmsigzz5hZDRk"
+
+# بيانات الدفع
+PAYPAL_EMAIL = "mimoucanadien01@gmail.com"
+BARIDIMOB_NUMBER = "00799999001806537421"
+USDT_ADDRESS_1 = "0x1aaa53596e4a3fc411bdf5c8c9c315b2253bcbda"
+USDT_ADDRESS_2 = "0x4ec85ef2dca621ed2cdd0dbed7a9ee2d5d688f20"
+
+# الأسعار
+SUBSCRIPTION_PRICE_USD = "200$"
+SUBSCRIPTION_PRICE_DZD = "48000 Da"
